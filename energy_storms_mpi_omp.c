59bdb5db9559d1bc06afcd6869883410347d6936
Innujan <Innujan04@gmail.com>
2026-01-24 15:58:55 +0100

    undo of change on not 'core' file

--- energy_storms_mpi_omp.c
+++ energy_storms_mpi_omp.c
@@ -32,12 +32,9 @@ void core(int layer_size, int num_storms, Storm *storms, float *maximum, int *po
 int main(int argc, char *argv[]) {
     int i;
 
-    int rank, size, provided;
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
-    if (provided < MPI_THREAD_FUNNELED) {
-        printf("Error: The MPI library does not provide needed thread support\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
-    }
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
