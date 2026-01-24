2173d278ffbd1e64fc6732fdccf4a470de08136b
Innujan <Innujan04@gmail.com>
2026-01-24 15:55:54 +0100

    changed MPI_Init with MPI_Init_thread and MPI_THREAD_FUNNELED thread-level support

--- energy_storms_mpi_omp.c
+++ energy_storms_mpi_omp.c
@@ -32,9 +32,12 @@ void core(int layer_size, int num_storms, Storm *storms, float *maximum, int *po
 int main(int argc, char *argv[]) {
     int i;
 
-    int rank, size;
-
-    MPI_Init(&argc, &argv);
+    int rank, size, provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+    if (provided < MPI_THREAD_FUNNELED) {
+        printf("Error: The MPI library does not provide needed thread support\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
